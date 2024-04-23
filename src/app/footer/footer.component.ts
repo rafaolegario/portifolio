e48fecb34bc251fc07113bf0886ba3f79e618373
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2024-04-23 20:59:08 -0300

    Criaçao do footer e finalização do projeto

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
+import { Profile } from '../models/profile';
 
 @Component({
   selector: 'app-footer',
@@ -11,4 +12,11 @@ import { ProfileService } from '../services/profile.service';
 export class FooterComponent {
 
   constructor(private profileService: ProfileService){}
+
+  profile?:Profile
+  date: Date = new Date()
+
+  ngOnInit(){
+    this.profile = this.profileService.getProfile()
+  }
 }
